9fbf759ce70cf93937a3eb4e233bf48a7ba87995
marnixah <marnix@hagehoeve.com>
2021-06-22 12:46:29 +0200

    feat(positivity): encouraging positive behaviour

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import { Client } from "@typeit/discord"
 import assert from "assert"
+import { positivityEncourager } from "./modules/positivityEncourager"
 import { theGreatFirewall } from "./modules/theGreatFirewall"
 
 const bot = new Client()
 
 bot.on('message', (msg) => {
   theGreatFirewall.onMessage(msg)
+  positivityEncourager.onMessage(msg)
 })
 
 bot.on('ready', () => {
   theGreatFirewall.startup(bot)
+  positivityEncourager.startup(bot)
 })
 
 const token = process.env.TOKEN
---
+++ modules/positivityEncourager.ts
@@ -0,0 +1,39 @@
+import { SentimentAnalyzer, PorterStemmer, WordTokenizer } from 'natural'
+import compromise from "compromise";
+import { positiveThings } from '../variables';
+import { Module } from 'interfaces/module';
+import { updateSocialCreditScore } from './socialCreditScore';
+import 'colorts/lib/string';
+
+const tokenizer = new WordTokenizer()
+const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn")
+
+export const positivityEncourager: Module = {
+  onMessage: async (msg) => {
+    if(!await isSentenceAboutPositiveThing(msg.content)) return
+    let amountOfPositivity = getAmountOfPositivity(msg.content)
+    let socialCreditChange = amountOfPositivity * 40
+    if (amountOfPositivity > 0) {
+      console.log(`[${'POSITIVITY'.green}] Positive message detected (${amountOfPositivity})`)
+      msg.channel.send(`好公民! +${socialCreditChange}社会信用`)
+    } else {
+      console.log(`[${'POSITIVITY'.green}] Negative message detected (${amountOfPositivity})`)
+      msg.channel.send(`坏公民! ${socialCreditChange}个社会信用`)
+    }
+    await updateSocialCreditScore(parseInt(msg.author.id), socialCreditChange)
+  },
+  startup: () => console.log(`[${'POSITIVITY'.green}] Initialized positivity!`)
+}
+
+const getAmountOfPositivity = (sentence: string) => analyzer.getSentiment(tokenizer.tokenize(sentence))
+
+const isSentenceAboutPositiveThing = (sentence: string) => {
+  return new Promise((resolve, _reject) => {
+    const sentenceCompromise = compromise(sentence)
+    sentenceCompromise.topics().forEach(topic => {
+      let positiveThing = positiveThings.find((thing) => topic.text().toLocaleLowerCase().includes(thing))
+      if(positiveThing) resolve(true)
+    });
+    resolve(false)
+  })
+}
