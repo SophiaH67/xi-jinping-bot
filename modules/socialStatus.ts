2e0ff0b3613c67bc0c2b33e0613ab1277066226b
marnixah <marnix@hagehoeve.com>
2021-06-23 12:29:14 +0200

    refactor(socialstatus): unneeded nested brackets

--- modules/socialStatus.ts
+++ modules/socialStatus.ts
@@ -5,23 +5,22 @@ import { getUser } from './socialCreditScore'
 
 export const socialStatus: Module = {
   onMessage: async (msg) => {
-    if (msg.content.toLowerCase().startsWith('what is my score?')) {
-      const user = await getUser(
-        parseInt(msg.mentions.members?.first()?.id || msg.author.id)
+    if (!msg.content.toLowerCase().startsWith('what is my score?')) return
+    const user = await getUser(
+      parseInt(msg.mentions.members?.first()?.id || msg.author.id)
+    )
+    let logEmbed = new MessageEmbed()
+      .setColor('#FF0000')
+      .setTitle('社会信用评分')
+    const logs = user.log.slice(-10)
+    for (let i = 0; i < logs.length; i++) {
+      const log = logs[i]
+      logEmbed.addField(
+        `${log.change > 0 ? '+' : ''}${log.change}`,
+        `${log.reason}`
       )
-      let logEmbed = new MessageEmbed()
-        .setColor('#FF0000')
-        .setTitle('社会信用评分')
-      const logs = user.log.slice(-10)
-      for (let i = 0; i < logs.length; i++) {
-        const log = logs[i]
-        logEmbed.addField(
-          `${log.change > 0 ? '+' : ''}${log.change}`,
-          `${log.reason}`
-        )
-      }
-      msg.channel.send(`${user.socialCreditScore}`, { embed: logEmbed })
     }
+    msg.channel.send(`${user.socialCreditScore}`, { embed: logEmbed })
   },
   startup: () => {
     console.log(`[${'SOCIALSTATUS'.yellow}] ${generatePropaganda()}`)
