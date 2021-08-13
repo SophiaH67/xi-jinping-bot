2f4c00018d996da11678a90c6a0888ac2a837b00
marnixah <marnix@hagehoeve.com>
2021-08-13 22:07:09 +0200

    feat(messageSender): added embed support

--- modules/messageSender.ts
+++ modules/messageSender.ts
@@ -1,4 +1,4 @@
 import { DMChannel, MessageEmbed, NewsChannel, TextChannel } from "discord.js"
 
-export const sendMessage = (channel: TextChannel | DMChannel | NewsChannel, msg: string | MessageEmbed) =>
-  channel.send(msg).catch(_=>{})
+export const sendMessage = (channel: TextChannel | DMChannel | NewsChannel, msg: string, embed?: MessageEmbed) =>
+  embed ? channel.send(msg, {embed: embed}).catch(_=>{}) : channel.send(msg).catch(_=>{})
