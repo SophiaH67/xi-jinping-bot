96ca8e1f9d62fc6d96bb5d76f4eba309b3917f84
marnixah <marnix@hagehoeve.com>
2021-08-25 01:12:50 +0900

    feat(xiBlessing): gave everyone access to rm data

--- modules/xisBlessing.ts
+++ modules/xisBlessing.ts
@@ -1,30 +1,20 @@
 import { Module } from '../interfaces/module'
 import 'colorts/lib/string'
 import { generatePropaganda } from './messageGenerator'
-import { owners } from '../variables'
-import { getUser, updateSocialCreditScore } from './socialCreditScore'
+import { getUser } from './socialCreditScore'
 import { sendMessage } from './messageSender'
 
 export const xisBlessing: Module = {
   onMessage: async (msg) => {
-    if (!owners.includes(msg.author.id)) return
     if (!msg.content.startsWith('xi bless ')) return
-    const target = parseInt(msg.mentions.members?.first()?.id || msg.author.id)
+    const target = parseInt(msg.author.id)
     const citizen = await getUser(target)
-    await updateSocialCreditScore(
-      target,
-      0 - (citizen.socialCreditScore as number) + 1000,
-      '伟大的霸主习近平保佑你的灵魂'
-    )
-    sendMessage(msg.channel, '伟大的霸主习近平保佑你的灵魂')
+    setTimeout(async () => {
+      citizen.remove()
+      await sendMessage(msg.channel, '伟大的霸主习近平保佑你的灵魂')
+    }, 5000)
   },
   startup: () => {
-    console.log(
-      `[${"XI'S BLESSING".magenta}] ${
-        owners.length == 0
-          ? 'OWNER_IDS is not set, module will not work'
-          : generatePropaganda()
-      }`
-    )
+    console.log(`[${"XI'S BLESSING".magenta}] ${generatePropaganda()}`)
   },
 }
