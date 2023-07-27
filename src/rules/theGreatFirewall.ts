08c10faa9649ae923ed700687fe74048be516e9a
Marni <marnix@hagehoeve.com>
2023-07-27 09:10:04 +0200

    add social credit score values to firewall return strings

--- src/rules/theGreatFirewall.ts
+++ src/rules/theGreatFirewall.ts
@@ -6,12 +6,12 @@ export default async function theGreatFirewall({
   message,
 }: RuleArgs): Promise<RuleReturn> {
   let badWord = bannedWords.find((bad) => message.includes(bad))
-  if (badWord) return [`${badWord}不存在!`, -100]
+  if (badWord) return [`${badWord}不存在！ -100 社交点`, -100]
 
   let fakeThing = fakeThings.find((fake) => message.includes(fake))
-  if (fakeThing) return [`${fakeThing}是由外国资本家猪组成的。`, -200]
+  if (fakeThing) return [`${fakeThing}是外国资本家猪编出来的！ -200社会信用点！`, -200]
 
   let offensiveThing = offensive.find((offense) => message.includes(offense))
-  if (offensiveThing) return ['CCP不赞成你的存在。', -500]
+  if (offensiveThing) return ['CCP不赞成你的存在。-500 社交点', -500]
   return
 }
