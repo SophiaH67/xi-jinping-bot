611a9a72a9ff7e5e02d30ec9cfe046847aa10c45
Sophia Hage <sophia@sophiah.gay>
2024-01-27 18:20:39 +0100

    Move backend code to separate class

--- packages/xi-jinping-bot/XiJinpingBot.cs
+++ packages/xi-jinping-bot/XiJinpingBot.cs
@@ -1,52 +1,24 @@
-using System.Net.Http.Headers;
 using Discord;
 using Discord.WebSocket;
-using Newtonsoft.Json;
 
 public class XiJinpingBot : DiscordSocketClient
 {
-  private readonly HttpClient httpClient = new();
+  private readonly Backend _backend;
 
   public XiJinpingBot() : base(new DiscordSocketConfig
   {
     GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent | GatewayIntents.DirectMessages,
   })
   {
-    Log += this.LogF;
+    Log += LogF;
     MessageReceived += MessageReceivedF;
+    _backend = new Backend(new HttpClient());
   }
 
   private Task LogF(LogMessage msg)
   {
     Console.WriteLine(msg.ToString());
     return Task.CompletedTask;
-  }
-
-  private async Task<string[]> SendMessageToBackend(SocketMessage message)
-  {
-    XiJinpingMessageDto dto = await XiJinpingMessageDto.FromMessage(message);
-
-    HttpRequestMessage request = new(HttpMethod.Post, "http://localhost:3000/check");
-
-    request.Content = new StringContent(JsonConvert.SerializeObject(dto));
-    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-    HttpResponseMessage response = await httpClient.SendAsync(request);
-
-    if (response.IsSuccessStatusCode == false)
-    {
-      throw new FailedToSendMessageException(response.StatusCode, await response.Content.ReadAsStringAsync());
-    }
-
-    string responseJson = await response.Content.ReadAsStringAsync();
-    XiJinpingResponseDto? responseDto = JsonConvert.DeserializeObject<XiJinpingResponseDto>(responseJson);
-
-    if (responseDto?.Messages == null)
-    {
-      throw new FailedToDeserializeException(responseJson);
-    }
-
-    return responseDto.Messages;
   }
 
   private async Task MessageReceivedF(SocketMessage message)
@@ -56,7 +28,7 @@ public class XiJinpingBot : DiscordSocketClient
       return;
     }
 
-    string[] messages = await SendMessageToBackend(message);
+    string[] messages = await _backend.ProcessMessage(message);
 
     foreach (string messageToSend in messages)
     {
